83d998ba890ab70b61a26a3c403358cc47e7ff13
xinfeng <2926272975@qq.com>
2019-03-29 14:31:12 +0800

    修改注释说明

--- common/src/main/java/com/study/ip/GetIpAddress.java
+++ common/src/main/java/com/study/ip/GetIpAddress.java
@@ -16,27 +16,36 @@ public class GetIpAddress {
     }
 
     public static InetAddress getLocalIp() throws SocketException, UnknownHostException {
+        /**
+         * 候选IP地址
+         */
         InetAddress candidateAdderss = null;
+        /**
+         * 获取所有网络接口
+         */
         Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
         while (networkInterfaces.hasMoreElements()) {
 
-                NetworkInterface networkInterface = networkInterfaces.nextElement();
-                Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
 
-                while (inetAddresses.hasMoreElements()) {
-                    InetAddress inetAddress = inetAddresses.nextElement();
+            /**
+             * 获取接口下的所有IP地址
+             */
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
 
 
-                    if (!inetAddress.isLoopbackAddress()&&inetAddress instanceof Inet4Address) {
-                        if (inetAddress.isSiteLocalAddress()) {
-                            //TODO  这里也可能有多个有效IP地址 立即返回就是选取了第一个IP
-                            System.out.println("real ip: " + inetAddress.getHostAddress());
-                            return inetAddress;
-                        }else if(candidateAdderss == null){
-                            candidateAdderss = inetAddress;
-                        }
+                if (!inetAddress.isLoopbackAddress()&&inetAddress instanceof Inet4Address) {
+                    if (inetAddress.isSiteLocalAddress()) {
+                        //TODO  这里也可能有多个有效IP地址 立即返回就是选取了第一个IP
+                        System.out.println("real ip: " + inetAddress.getHostAddress());
+                        return inetAddress;
+                    }else if(candidateAdderss == null){
+                        candidateAdderss = inetAddress;
                     }
                 }
+            }
         }
 
         if(candidateAdderss!=null){
@@ -44,9 +53,9 @@ public class GetIpAddress {
         }
 
         InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
-        if (jdkSuppliedAddress == null) {
-            throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
-        }
+//        if (jdkSuppliedAddress == null) {
+//            throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+//        }
         return jdkSuppliedAddress;
     }
 
