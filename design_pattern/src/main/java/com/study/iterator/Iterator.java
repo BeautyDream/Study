5f6416ee1187f75fb0c5f9e1217244ef8286113f
LiaoPeng <2926272975@qq.com>
2019-05-10 11:36:08 +0800

    添加注释

--- design_pattern/src/main/java/com/study/iterator/Iterator.java
+++ design_pattern/src/main/java/com/study/iterator/Iterator.java
@@ -3,7 +3,7 @@ package com.study.iterator;
 /**
  * @Auther: LiaoPeng
  * @Date: 2019/5/10
- * 简单的迭代器接口
+ * 简单的迭代器接口  作用：将实现和迭代分离开来
  */
 public interface Iterator {
     public abstract boolean hasNext();
--- design_pattern/src/main/java/com/study/iterator/Main.java
+++ design_pattern/src/main/java/com/study/iterator/Main.java
@@ -3,6 +3,7 @@ package com.study.iterator;
 /**
  * @Auther: LiaoPeng
  * @Date: 2019/5/10
+ * 迭代器模式 测试
  */
 public class Main {
     public static void main(String[] args) {
